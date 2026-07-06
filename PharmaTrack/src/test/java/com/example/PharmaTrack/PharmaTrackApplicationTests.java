145a5b123a37dd7399e93526a27107a2747ea3ea
Mathewos K <mathewos1@yahoo.com>
2026-07-06 21:48:41 +0300

    initializer

---
+++ PharmaTrack/src/test/java/com/example/PharmaTrack/PharmaTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PharmaTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
